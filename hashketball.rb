749405abf405702f6d66d5fb7d25a7d860aa66d6
Brian Li <brian.li96@yahoo.com>
2020-11-08 07:08:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,15 @@
 # Write your code here!
 def game_hash
-  
+  hashket = {
+   :home => {
+     :team_name => " ",
+     :colors => [],
+     :players => [{}]
+   }
+   :away => {
+     :team_name => " ",
+     :colors => [],
+     :players => [{}]
+   }
+  }
 end
