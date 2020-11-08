f3f1c66eb4a3fb001859f1d7ff727858be3f77d4
Brian Li <brian.li96@yahoo.com>
2020-11-08 07:24:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,7 +16,7 @@ def game_hash
      :team_name => "Charlotte Hornets",
      :colors => ["Turquoise", "Purple"],
      :players => [
-       {:player_name => "", :number =>  , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => },
+       {:player_name => "Jeff Adrien", :number =>  , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => },
        {:player_name => "", :number =>  , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => },
        {:player_name => "", :number =>  , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => },
        {:player_name => "", :number =>  , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => },
